53610658aaa98989385e2c1ab00f3d71ca18166a
JakubGrodzicki <jakubgrodzicki@protonmail.com>
2022-04-25 18:23:35 +0200

    Poprawiona nazwa folderów

---
+++ jamelly-frontend/src/App.js
@@ -0,0 +1,25 @@
+
+function App() {
+  return(
+  <div>
+      <h1>
+        <p id="Jam">Jam</p>
+        <p id="elly">elly</p>
+      </h1>
+      <div id="Rectangle2">
+        <div id="Rectangle1">
+          <p id="mail">adres email</p>
+        </div>
+        <div id="Rectangle3">
+          <p id="pass">hasło</p>
+        </div>
+        <button id="Login">
+          <p id="LoginText">Zaloguj</p>
+        </button>
+      </div>
+      <h6>Zapomniałeś hasła?</h6>
+      </div>
+  );
+}
+
+export default App;
