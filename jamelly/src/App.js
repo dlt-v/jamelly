ae9e6cb40c43edbdbb7076f28d11513b670b6958
JakubGrodzicki <jakubgrodzicki@protonmail.com>
2022-04-25 17:36:01 +0200

    Czysty HTML wersja ekranu logowania Jamelly
    Dodałem to bo wygląda ładniej niż biała strona

--- jamelly/src/App.js
+++ jamelly/src/App.js
@@ -1,6 +1,24 @@
+
 function App() {
-  return (
-    <h1>Tu będzie apka Jamelly</h1>
+  return(
+  <div>
+      <h1>
+        <p id="Jam">Jam</p>
+        <p id="elly">elly</p>
+      </h1>
+      <div id="Rectangle2">
+        <div id="Rectangle1">
+          <p id="mail">adres email</p>
+        </div>
+        <div id="Rectangle3">
+          <p id="pass">hasło</p>
+        </div>
+        <button id="Login">
+          <p id="LoginText">Zaloguj</p>
+        </button>
+      </div>
+      <h6>Zapomniałeś hasła?</h6>
+      </div>
   );
 }
 
