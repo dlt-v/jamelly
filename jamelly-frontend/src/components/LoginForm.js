1cb3f9198f357d6504da2bb7a951a788018c6496
JakubGrodzicki <jakubgrodzicki@protonmail.com>
2022-04-26 19:22:53 +0200

    Menu logowania wraz ze stroną powitalną
    Strona powitalna zawiera Dropdown Menu (jak na razie nie podłączone do niczego konkretnego). Dodatkowo, dropdown menu wymaga dodatkowego formatowania.

--- jamelly-frontend/src/components/LoginForm.js
+++ jamelly-frontend/src/components/LoginForm.js
@@ -43,7 +43,7 @@ function LoginForm({ Login, error }) {
           <input type="submit" value="Zaloguj" id="Login" />
         </div>
       </div>
-      <h6>Zapomniałeś hasła?</h6>
+      <h6><a href="#">Zapomniałeś hasła?</a></h6>
     </form>
   );
 }
