84b82a366e0d844a15831ed4f4108fbc6a9094a3
Delta.v <tmm.michalski@gmail.com>
2022-05-17 00:58:06 +0200

    feat: create listing and creating view for todo

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, authentication
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('name')
+        serializer.save()
