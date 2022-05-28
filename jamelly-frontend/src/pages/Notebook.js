6616c2e67cdce3921168e66a95487c4dfe8e4b64
JakubGrodzicki <jakubgrodzicki@protonmail.com>
2022-05-28 18:22:27 +0200

    wstępna implementacja notebook (poprawiona)

--- jamelly-frontend/src/pages/Notebook.js
+++ jamelly-frontend/src/pages/Notebook.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
-import {useState} from "react";
+import { useState } from "react";
 import uuid from "react-uuid";
 
+import Sidebar from "../components/Notebook-Sidebar";
+import Main from "../components/Notebook-Main";
+
 function NotebookPage() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+
+  const [activeNote, setActiveNote] = useState(false);
+
 
   const onAddNote = () => {
     const newNote = {
@@ -12,56 +20,49 @@ function NotebookPage() {
       title: "Untitled Note",
       body: "",
       lastModified: Date.now(),
-    }
-    setNotes([newNote,...notes]);
-  }
-  const onDeleteNote = (deleteId) => {
-    setNotes(notes.filter((note)=>note.id !== deleteId));
-  }
-  const [activeNote, setActiveNote]=useState(false);
+    };
 
+    setNotes([newNote, ...notes]);
+  };
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete));
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNotesArr);
+  };
 
   const getActiveNote = () => {
-    return notes.find((note)=> note.id ===activeNote);
+    return notes.find(({ id }) => id === activeNote);
   };
-const onEditField = (key,value)=>{
-  onUpdateNote({
-    ...activeNote,
-    [key]: value,
-    lastModified: Date.now(),
-  })
-}
-const onUpdateNote = (updatedNote)=>{
-  const updatedNotesArray = notes.map((note)=>{if(note.id===activeNote){return updatedNote;}
-return note;})
-setNotes(updatedNotesArray)
-};
-
-
-
-
 
   return (
-    <div
+    <motion.div
       className="NotebookMain"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
     >
-      <div id="Sidebar">
-        <div id="Sidebar-Header"><h1>Notes</h1><button onClick={onAddNote}>ADD</button></div>
-        <div id="All-Notes-Sidebar"> {notes.map((note)=>(<div className={`Note-Sidebar ${note.id === activeNote && "active"}`} onClick={()=>setActiveNote(note.id)}><div id="Note-Title"><strong>{note.title}</strong>
-        <button onClick={()=>onDeleteNote(note.id)}>DELETE</button></div>
-        <p>{note.body && note.body.substr(0,100)+"..."}</p><small className="note-meta">Last modified {new Date(note.lastModified).toLocaleDateString("pl-PL",{
-          hour:"2-digit", minute: "2-digit",})}</small></div>))}
-         
-        </div>
-      </div>
-      
-      
-        <div id="NotebookSpace">
-          <div id="NoteEdit"><input type="text" id="title" placeholder="Note Title" value={activeNote.title} onChange={(e)=> onEditField("title",e.target.value)} autoFocus />
-          <textarea id="body" placeholder="Lorem Ipsum..." value={activeNote.body} onChange={(e)=> onEditField("body",e.target.value)}/>
-          </div>
-        </div>
-    </div>
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+
+      <Main activeNote={getActiveNote()} />
+    </motion.div>
   );
 }
 
