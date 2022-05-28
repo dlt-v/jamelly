eaecd86ae73b3ea362fe66079fbffeb257d04bd9
JakubGrodzicki <jakubgrodzicki@protonmail.com>
2022-05-28 16:16:36 +0200

    Kopia strony

--- jamelly-frontend/src/pages/Notebook.js
+++ jamelly-frontend/src/pages/Notebook.js
@@ -1,20 +1,67 @@
 import React from "react";
 import { motion } from "framer-motion";
+import {useState} from "react";
+import uuid from "react-uuid";
 
 function NotebookPage() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    }
+    setNotes([newNote,...notes]);
+  }
+  const onDeleteNote = (deleteId) => {
+    setNotes(notes.filter((note)=>note.id !== deleteId));
+  }
+  const [activeNote, setActiveNote]=useState(false);
+
+
+  const getActiveNote = () => {
+    return notes.find((note)=> note.id ===activeNote);
+  };
+const onEditField = (key,value)=>{
+  onUpdateNote({
+    ...activeNote,
+    [key]: value,
+    lastModified: Date.now(),
+  })
+}
+const onUpdateNote = (updatedNote)=>{
+  const updatedNotesArray = notes.map((note)=>{if(note.id===activeNote){return updatedNote;}
+return note;})
+setNotes(updatedNotesArray)
+};
+
+
+
+
+
   return (
-    <motion.div
+    <div
       className="NotebookMain"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
     >
-        <div id="NotebookPlaceholder">
-          <p id="Lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum, felis eu mollis convallis, erat elit auctor massa, eget accumsan dolor nibh vel nunc. Fusce nec facilisis neque. Nulla finibus consectetur erat, et vulputate risus tincidunt a. Proin enim metus, cursus eu enim eu, imperdiet congue orci. Ut in volutpat felis. 
-Cras ornare ullamcorper nunc, eget tincidunt sapien lobortis ac. Sed consequat commodo nulla vel blandit. Integer ut sapien sit amet justo rutrum bibendum. Sed aliquam felis vel lectus tincidunt volutpat. Duis placerat, justo nec sollicitudin rhoncus, sem nisi posuere lacus, eu aliquam neque nulla sed justo. Vivamus</p>
+      <div id="Sidebar">
+        <div id="Sidebar-Header"><h1>Notes</h1><button onClick={onAddNote}>ADD</button></div>
+        <div id="All-Notes-Sidebar"> {notes.map((note)=>(<div className={`Note-Sidebar ${note.id === activeNote && "active"}`} onClick={()=>setActiveNote(note.id)}><div id="Note-Title"><strong>{note.title}</strong>
+        <button onClick={()=>onDeleteNote(note.id)}>DELETE</button></div>
+        <p>{note.body && note.body.substr(0,100)+"..."}</p><small className="note-meta">Last modified {new Date(note.lastModified).toLocaleDateString("pl-PL",{
+          hour:"2-digit", minute: "2-digit",})}</small></div>))}
+         
         </div>
-    </motion.div>
+      </div>
+      
+      
+        <div id="NotebookSpace">
+          <div id="NoteEdit"><input type="text" id="title" placeholder="Note Title" value={activeNote.title} onChange={(e)=> onEditField("title",e.target.value)} autoFocus />
+          <textarea id="body" placeholder="Lorem Ipsum..." value={activeNote.body} onChange={(e)=> onEditField("body",e.target.value)}/>
+          </div>
+        </div>
+    </div>
   );
 }
 
