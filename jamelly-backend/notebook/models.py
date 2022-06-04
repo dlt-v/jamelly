569ba0c8d0eb441922923a9805b374fcd82689da
dlt-v <tmm.michalski@gmail.com>
2022-06-04 19:43:23 +0200

    feat: add database models

--- jamelly-backend/notebook/models.py
+++ jamelly-backend/notebook/models.py
@@ -1,3 +1,16 @@
+from re import M
 from django.db import models
 
 # Create your models here.
+
+
+class Notebook(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, blank=True, default='')
+    # owner_id =
+
+
+class NoteSnippet(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=1000, blank=False)
+    # notebook_id
