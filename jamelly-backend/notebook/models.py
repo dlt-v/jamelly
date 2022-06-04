a56bfe461b976c0f06b15d0c06ba240c6a0abd36
dlt-v <tmm.michalski@gmail.com>
2022-06-04 19:48:06 +0200

    chore: apply migrations for notebook

--- jamelly-backend/notebook/models.py
+++ jamelly-backend/notebook/models.py
@@ -1,4 +1,3 @@
-from re import M
 from django.db import models
 
 # Create your models here.
@@ -9,8 +8,14 @@ class Notebook(models.Model):
     name = models.CharField(max_length=50, blank=True, default='')
     # owner_id =
 
+    class Meta:
+        ordering = ['name']
+
 
 class NoteSnippet(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=1000, blank=False)
     # notebook_id
+
+    class Meta:
+        ordering = ['created_at']
