04c3a464ce6ce29d0c94d4d8b6f28fe4263e59f3
dlt-v <tmm.michalski@gmail.com>
2022-06-05 17:59:08 +0200

    fix: update urls.py

--- jamelly-backend/notebook/urls.py
+++ jamelly-backend/notebook/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from notebook import views
 
-urlpattern = [
-    path('notebooks/', views.notebook_list),
-    path('notebooks/<int:pk>', views.notebook_detail),
+urlpatterns = [
+    path('notebooks/', views.NotebookList.as_view()),
+    path('notebooks/<int:pk>/', views.NotebookDetail.as_view()),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
