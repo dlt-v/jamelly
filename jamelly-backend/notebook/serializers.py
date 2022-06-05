450ea2893029defef975c73ad3939dcb3b3317b4
Delta.v <tmm.michalski@gmail.com>
2022-06-05 23:13:23 +0200

    feat: add user serializer to show notebooks

--- jamelly-backend/notebook/serializers.py
+++ jamelly-backend/notebook/serializers.py
@@ -1,11 +1,28 @@
 from rest_framework import serializers
 from notebook.models import Notebook, NoteSnippet
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    notebooks = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Notebook.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'notebooks']
 
 
 class NotebookSerializer(serializers.ModelSerializer):
+    note_snippets = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Notebook.objects.all()
+    )
+
     class Meta:
         model = Notebook
-        fields = ['id', 'name', 'created_at']
+        fields = ['id', 'name', 'created_at', 'note_snippets']
 
 
 class NoteSnippetSerializer(serializers.ModelSerializer):
