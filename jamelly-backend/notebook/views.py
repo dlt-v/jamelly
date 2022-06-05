37690940be5b8f0284686e0a8119124882d3d5d2
dlt-v <tmm.michalski@gmail.com>
2022-06-05 17:51:05 +0200

    feat: add NotebookDetail class

--- jamelly-backend/notebook/views.py
+++ jamelly-backend/notebook/views.py
@@ -1,3 +1,4 @@
+from notebook import serializers
 from notebook.models import Notebook, NoteSnippet
 from notebook.serializers import NotebookSerializer, NoteSnippetSerializer
 from django.http import Http404
@@ -22,3 +23,36 @@ class NotebookList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class NotebookDetail(APIView):
+    '''
+    Retrieve, edit, or delete a specific notebook instance.
+    '''
+
+    def get_object(self, pk):
+        '''
+        Tries to find an existing object.
+        '''
+        try:
+            return Notebook.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        notebook = self.get_object(pk)
+        serializer = NotebookSerializer(notebook)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        notebook = self.get_object(pk=pk)
+        serializer = NotebookSerializer(notebook, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        notebook = self.get_object(pk=pk)
+        notebook.delete()
+        return Response(status=status.HTTP_200_OK)
