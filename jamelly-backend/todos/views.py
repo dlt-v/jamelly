f939163af7df94a13a2ab4b0479bb8e2413d413d
Delta.v <tmm.michalski@gmail.com>
2022-06-08 16:39:56 +0200

    chore: refactor views.py in Todos

--- jamelly-backend/todos/views.py
+++ jamelly-backend/todos/views.py
@@ -1,2 +1,20 @@
 
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from todos.models import Todo
+from todos.serializers import TodoSerializer
+
+
+class TodoList(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner_id=self.request.user)
+
+
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
