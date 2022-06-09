469f9824d1b0bab8deac98b910852a96e99c8d9b
Delta.v <tmm.michalski@gmail.com>
2022-06-09 22:41:47 +0200

    feat: add custom user registration serializer

---
+++ jamelly-backend/users/serializers.py
@@ -0,0 +1,36 @@
+
+from rest_framework import serializers
+
+from django.contrib.auth import models
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True
+    )
+
+    class Meta:
+        model = models.User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+
+        new_user = models.User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'],
+        )
+
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError(
+                {'password': 'Passwords do not match.'}
+            )
+
+        new_user.set_password(password)
+        new_user.save()
