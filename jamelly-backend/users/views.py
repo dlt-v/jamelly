0d7ddef265ab476336b4a9a3eb4936d1a237c736
Delta.v <tmm.michalski@gmail.com>
2022-06-13 17:48:48 +0200

    feat: add change password view

--- jamelly-backend/users/views.py
+++ jamelly-backend/users/views.py
@@ -1,6 +1,10 @@
-from users.serializers import RegistrationSerializer
+from notebook import serializers
+from users.serializers import ChangePasswordSerializer, RegistrationSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, authentication, status
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import models
 
 
 @api_view(['POST'])
@@ -18,3 +22,38 @@ def registration_view(request):
             data = serializer.errors
 
         return Response(data)
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = models.User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
